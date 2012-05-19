4d46548e1d24ed9dde61321f4be63eca2170af30
ckamsler <chriskamsler@gmail.com>
2012-05-19 18:25:27 -0400

    updates

---
+++ src/html/js/api.js
@@ -0,0 +1,28 @@
+(function($){
+
+	var Kover = function() {};	
+
+	Kover.prototype = {
+	
+		constructor: Kover,
+
+		fb: {},
+
+		usat: {},
+
+		whitli: {	
+			populate: function(uid){
+			
+					
+
+			}
+		}
+
+	};	
+
+
+
+	window.api = new Kover;
+
+
+})(jQuery);
