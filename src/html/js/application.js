dc778b623b50fdb4e4fa5ede56da293bc766d24f
ckamsler <chriskamsler@gmail.com>
2012-05-19 23:50:05 -0400

    updates

--- src/html/js/application.js
+++ src/html/js/application.js
@@ -1,43 +1,84 @@
 (function($) {
    'use strict';
 
-	var _state = window.location;
+	var $yield = $("#yield");
 
-	$(function() {
+	// should be inherited from a root Page class, but i don't care...
 
-		api.usat.getCategories(function(categories) {
+	var FriendsView = {
 
-			api.usat.getBooksListByCategory("horror", function(list) {
+		destroy: function() {
+			$yield.find(".friends").unbind("click");
+		},
 
-					var $out = $("#out"), html = ["<ul>"];	
+		go: function() {
+			
+			_page = this;
+		
+			template.render("friends.html", $yield, function() {
 
-					list.forEach(function(book) {
-					
-						html.push("<li>", book.Title, " - ", book.ISBN, "</li>");
-					
-					});
+				$yield.find(".friends").bind("click", function(e) {
 
-					html.push("</ul>");
+					_page.destroy();
 
-					$out.html(html.join(""));
+					BookShelfView.go();
 
-					$("#out ul").click(function() {
+				});			
 
-						var page = "reading-view.html";
+			});
 
-						template.render(page, $("#out"), function() {
+		}
+	};
+
+	var BookShelfView = {	
+		destroy: function() {
+			$("#shelf").find("li").unbind("click");
+		},
+		go: function() {
+		
+			_page = this;
+
+			template.render("bookshelf.html", $yield, function() {
+			
+				api.usat.getCategories(function(categories) {
+
+					api.usat.getBooksListByCategory("horror", function(list) {
+
+						var $shelf = $("#shelf"), html = ["<ul>"];	
+
+						list.forEach(function(book) {
 						
-							_state = page;
+							html.push("<li>", book.Title, " - ", book.ISBN, "</li>");
+					
+						});
 
+						html.push("</ul>");
+
+						$shelf.html(html.join(""));
+
+						$shelf.find("li").bind("click", function() {
+
+							_page.destroy();
+						
 						});
 		
 					});
-
 				
+				});
+		
 			});
+			
+		}
 		
-		});
+	};
+
+
+	var _state = window.location, _page = null;
+
+	$(function() {
 		
+		FriendsView.go();
+	
 	});
 
 })(jQuery);
